72372ef701728c8b258f4139eb4f198a7c644367
Lays Hacmuhammed <gays.haj.mohamad.12345@gmail.com>
2025-01-03 23:59:21 +0300

    full

--- services/soruService.js
+++ services/soruService.js
@@ -1,22 +1,6 @@
 const {PrismaClient} = require('@prisma/client');
 const prisma = new PrismaClient();
 
-// (async function() {
-
-//   for (let i = 0; i < 90; i++) {
-//     const a = `genel A ${i} genel A ${i} genel A ${i}`;
-//     const b = `genel B ${i} genel B ${i} genel B ${i}`;
-//     const c = `genel C ${i} genel C ${i} genel C ${i}`;
-//     const d = `genel D ${i} genel D ${i} genel D ${i}`;
-//     const cat = "genel";
-//     const dogru = i % 4 == 0 ? 'a' : i % 4 == 1 ? 'b' : i % 4 == 2 ? 'c' : 'd';
-//     const soru = `${dogru} -- genel ${i} genel ${i} genel ${i} genel ${i} genel ${i} genel ${i} genel ${i} genel ${i} genel ${i} genel ${i}`
-//     await olustur(soru,a,b,c,d,dogru,cat);
-//     console.log(`${cat} - ${i}`);
-//   }
-
-// })()
-
 async function olustur(soru, a, b, c, d, dogruCevap, kategori, resim) {
   const yeniSoru = await prisma.soru.create({
     data: {
